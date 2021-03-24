42a20d2a65f9297fcb6529e6ecebbf08172643a7
Nabhan Abdulla <nabhan@criodo.com>
2021-03-24 21:21:22 +0550

    updated changes - 2021-03-24 21:01:22

--- solution.js
+++ solution.js
@@ -30,7 +30,7 @@ async function getAllProblems(apiUrl) {
 function getAllProblemRowElements() {
 }
 
-/* Step 5: Adding total_acs / total_submitted to each row element of the table on the page. Iterate through each row element and add a new <td> containing the submission data in the provided format
+/* Step 5: Add "total_acs"/"total_submitted" to each row element of the table on the page. Iterate through each row element and add a new <td> containing the submission data in the provided format
  */
 function addSubmissionsToEachProblem(allProblemRowElements, allProblems) {
 }
